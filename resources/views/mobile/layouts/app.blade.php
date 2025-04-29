65df69149d921f78bbc629986848e933f7682c2e
ardykaaw <151797651+ardykaaw@users.noreply.github.com>
2025-04-30 01:47:33 +0800

    perbaikan lagi

--- resources/views/mobile/layouts/app.blade.php
+++ resources/views/mobile/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="msapplication-starturl" content="/mobile/dashboard">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- PWA Icons -->
     <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('icons/icon-512x512.png') }}">
